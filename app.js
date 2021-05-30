eabb739ab5bdca714f3300c2f46f1dde231deda7
vUnAmp <phanhaingoc@gmail.com>
2021-05-30 22:53:36 +0200

    MVC : cover

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ require('dotenv').config();
 
 // Declare app
 const app = express();
+// express v4.16 or higher already includes body-parse
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'src', 'assets')));
 app.use(express.static(path.join(__dirname, 'src', 'resources')));
@@ -25,17 +28,26 @@ app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'src', 'resources', 'views'));
 
-app.use('/', (req, res, next) => {
-  console.log('this is middleware1');
-  next();
-});
-app.use('/', (req, res, next) => {
-  console.log('this is middleware2');
-  next();
-});
+// app.use('/', (req, res, next) => {
+//   console.log('this is middleware1');
+//   next();
+// });
+// app.use('/', (req, res, next) => {
+//   console.log('this is middleware2');
+//   next();
+// });
 app.get('/', (req, res) => {
   res.render('home');
   //   res.redirect('https://facebook.com');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  console.log(req.body.email);
+  res.render('login');
 });
 
 app.listen(process.env.PORT, () => {
